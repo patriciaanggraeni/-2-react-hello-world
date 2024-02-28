87da0d698832994b8e1a1fdcc2eeebc98408afcd
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-02-28 19:32:26 +0700

    Week 03: praktikum 2 langkah 1 - 2

--- 03-belajar-komponen/src/app/page.tsx
+++ 03-belajar-komponen/src/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 // import komponen profile
 import Profile from "../components/profile";
 
+// import komponen gallery
+import { Gallery } from "@/components/gallery";
+
 export default function Home() {
   return (
 
@@ -11,10 +14,12 @@ export default function Home() {
       <p className="mt-5 capitalize text-2xl flex justify-center">
         ilmuwan yang luar biasa
       </p>
-      <div className="mt-3 flex justify-evenly items-center">
-        <Profile />
-        <Profile />
-        <Profile />
+      <div className="mt-3 flex justify-center items-center">
+        {/* <Profile /> */}
+        {/* <Profile /> */}
+        {/* <Profile /> */}
+
+        <Gallery />
       </div>
     </main>
   );
