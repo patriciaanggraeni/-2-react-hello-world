1b16fb293a9680137e0b31270d31221441a16810
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-02-28 15:52:07 +0700

    Weeke 03: praktikum 1 langkah 1 - 3

---
+++ 03-belajar-komponen/src/components/profile.tsx
@@ -0,0 +1,21 @@
+// import terlebih dahulu Image dari next/image
+import Image from "next/image";
+
+// buat fungsi untuk menampilkan profile
+export default function Profile() {
+    return (
+    
+    // panggil tag image di sini
+    <Image
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+        width={100}
+        height={100}
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+          margin: "13px"
+        }}
+      />
+    );
+  }
