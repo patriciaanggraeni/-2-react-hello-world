8962cc60f9a831a8e89f79342165fe812c0a0381
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-02-28 14:06:20 +0700

    change directory

--- 02-react-hello-world/src/app/components/profile.tsx
+++ 02-react-hello-world/src/app/components/profile.tsx
@@ -13,11 +13,11 @@ const Profile = () => {
     return (
         <div className='text-center'>
             <Image
-            className="avatar rounded-full mx-auto mt-5"
-            src={user.imageUrl}
-            alt={'Foto ' + user.name}
-            width={ user.imageSize }
-            height={ user.imageSize } />
+                className="avatar rounded-full mx-auto mt-5"
+                src={user.imageUrl}
+                alt={'Foto ' + user.name}
+                width={ user.imageSize }
+                height={ user.imageSize } />
             <h1 className='mt-10 text-4xl font-bold'>{user.name}</h1>
             <h2 className='mt-10 text-4xl font-bold'>{user.nim}</h2>
         </div>
